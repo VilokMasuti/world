afc1ade3b5ea8af74e1cb5a69db6841176d9375e
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-05-27 15:55:53 +0530

    layout

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,15 +1,9 @@
 "use client";
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
-
-// Also install this npm i --save-dev @types/react-lottie
 import Lottie from "react-lottie";
-
 import { cn } from "@/utils/cn";
 import animationData from '../../Data/confetti.json';
-
-
-
 import MagicButton from "../../components/ui/MagicButon";
 import { Vortex } from "../ui/vortex";
 import GridGlobe from "./GridGlobe";
@@ -25,7 +19,6 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
         "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
@@ -40,7 +33,6 @@ export const BentoGridItem = ({
   id,
   title,
   description,
-  //   remove unecessary things here
   img,
   imgClassName,
   titleClassName,
@@ -68,6 +60,7 @@ export const BentoGridItem = ({
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+
   const handleCopy = () => {
     const text = "vilokmasuti@outlook.com";
     navigator.clipboard.writeText(text);
@@ -81,98 +74,67 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        // You can generate the color from here https://cssgradient.io/
         background: "rgb(0,0,0)",
-        backgroundColor:
-          "radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(41,38,39,1) 100%)",
+        backgroundColor: "radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(41,38,39,1) 100%)",
       }}
     >
-      {/* add img divs */}
-      <div className={`${id === 6 && "flex justify-center"} h-full`}>
+      <div className={`${id === 6 ? "flex justify-center" : ""} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
+            // eslint-disable-next-line @next/next/no-img-element
             <img
               src={img}
               alt={img}
-              className={cn(imgClassName, "object-cover object-center ")}
+              className={cn(imgClassName, "object-cover object-center w-full h-full")}
             />
           )}
         </div>
-        <div
-          className={`absolute right-0 -bottom-5 ${
-            id === 5 && "w-full opacity-80"
-          } `}
-        >
+        <div className={`absolute right-0 -bottom-5 ${id === 5 ? "w-full opacity-80" : ""}`}>
           {spareImg && (
+            // eslint-disable-next-line @next/next/no-img-element
             <img
               src={spareImg}
               alt={spareImg}
-              //   width={220}
               className="object-cover object-center w-full h-full"
             />
           )}
         </div>
-{id === 1 && (
-  <Vortex>
-<div  className=" w-full h-full flex items-center justify-center group-hover/bento:translate-x-2 transition duration-700   font-serif">
-<h1 className=" lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] text-3xl text-white-200  text-center pt-56 "> I prioritize client collaboration, fostering open communication </h1>
-</div>
-
-  </Vortex>
-)}
-
-        {id === 6 && (
-          // add background animation , remove the p tag
-        
-            <div className=" flex items-center justify-center  text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-                <Meteors number={40} />
+        {id === 1 && (
+          <Vortex>
+            <div className="w-full h-full flex items-center justify-center group-hover/bento:translate-x-2 transition duration-700 font-serif">
+              <h1 className="lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] text-3xl text-white-200 text-center pt-56">
+                I prioritize client collaboration, fostering open communication
+              </h1>
             </div>
-         
+          </Vortex>
         )}
-
-        <div
-          className={cn(
-            titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-700   font-serif   relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
-          )}
-        >
-          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
+        {id === 6 && (
+          <div className="flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+            <Meteors number={40} />
+          </div>
+        )}
+        <div className={cn(titleClassName, "group-hover/bento:translate-x-2 transition duration-700 font-serif relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10")}>
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
-          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
-          {/* remove mb-2 mt-2 */}
-          <div
-            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
-          >
+          <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
             {title}
           </div>
-          {/* for the github 3d globe */}
           {id === 2 && <GridGlobe />}
-          {/* Tech stack list div */}
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-              {/* tech stack lists */}
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                 {leftLists.map((item, i) => (
-                  <span
-                    key={i}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                  >
+                  <span key={i} className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                     {item}
                   </span>
                 ))}
-                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#10132E]"></span>
               </div>
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#10132E]"></span>
                 {rightLists.map((item, i) => (
-                  <span
-                    key={i}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                  >
+                  <span key={i} className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                     {item}
                   </span>
                 ))}
@@ -180,22 +142,16 @@ export const BentoGridItem = ({
             </div>
           )}
           {id === 6 && (
-            <div className=" mt-5 relative">
-              <div
-                className={` absolute -bottom-5 right-0 ${
-                  copied ? " block" : " hidden"
-                }`}
-              >
-                {/* <img src="/confetti.gif" alt="confetti" /> */}
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0 ${copied ? "block" : "hidden"}`}>
                 <Lottie options={defaultOptions} height={200} width={400} />
               </div>
               <MagicButton
-              
-              title={copied ? "Copied!" : " Email"}
-              icon={<IoCopyOutline />}
-              position="left"
-              handleClick={handleCopy}
-              otherClasses=" boder-none  "
+                title={copied ? "Copied!" : " Email"}
+                icon={<IoCopyOutline />}
+                position="left"
+                handleClick={handleCopy}
+                otherClasses="boder-none"
               />
             </div>
           )}
