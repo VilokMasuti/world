967638ab5f2d9c161aeba70615be1fe0cfb8989b
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-05-28 15:42:14 +0530

    now done

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -54,7 +54,7 @@ const Contact = () => {
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you achieve your goals.
         </p>
-        <div className='flex gap-11 max-sm:flex-col'>
+        <div className='flex gap-11 max-sm:flex-col max-sm:pt-5 max-sm:gap-12 max-sm:flex max-sm:ml-20 '>
         <div className="mt-5 relative">
               {/* button border magic from tailwind css buttons  */}
               {/* add rounded-md h-8 md:h-8, remove rounded-full */}
